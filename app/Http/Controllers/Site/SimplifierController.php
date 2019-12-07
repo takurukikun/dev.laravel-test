836f6aa05364fe934d9f2f09c8d2e2a384673d74
Levi Gleik <leviacedo1@gmail.com>
2019-12-07 09:10:07 -0300

    Parênteses corrigido

--- app/Http/Controllers/Site/SimplifierController.php
+++ app/Http/Controllers/Site/SimplifierController.php
@@ -7,50 +7,85 @@ use Illuminate\Support\Facades\Input;
 
 class SimplifierController extends Controller
 {
+
+    protected $limit;
+    protected $stack;
+    public function __construct($limit = 100, $initial = array()) {
+        $this->stack = $initial;
+        $this->limit = $limit;
+    }
+
+    public function push($item) {
+        if (count($this->stack) < $this->limit) {
+            array_unshift($this->stack, $item);
+        } else {
+            print('Stack is full!');
+        }
+    }
+
+     public function pop() {
+        if ($this->isEmpty()) {
+            throw new RunTimeException('Stack is empty!');
+        } else {
+            return array_shift($this->stack);
+        }
+    }
+
+    public function isEmpty() {
+        return empty($this->stack);
+    }
+
+    public function top() {
+        return current($this->stack);
+    }
+
+    public function match($character1, $character2) { 
+       if ($character1 == '(' && $character2 == ')'){
+         return true; 
+       }
+       else{
+         return false; 
+       }
+    } 
+
+    public function balanced($exp){ 
+        
+        for($i=0;$i<strlen($exp);$i++) { 
+            if ($exp[$i] == '('){
+                $this->push($exp[$i]); 
+            }
+            if ($exp[$i] == ')'){ 
+                if ($this->isEmpty()) { 
+                    return false; 
+                } elseif (!$this->match($this->pop(), $exp[$i]) ){ 
+                   return false; 
+                } 
+            } 
+        }
+        if ($this->isEmpty($this)){
+            return true;
+        }
+        else{
+            return false; 
+        }
+    }  
+
     public function simplified(Request $request){
-    	$exp = $request->input('expName');
+        $exp = $request->input('expName');
 
 
     //                    A+b*(b+a+c)
     //                    B+C*(a+b)+(b*c)+a
 
-    	//echo $exp;
-    	//echo $result;
-    	//print(substr_count($exp, ")"));
-    	if(substr_count($exp, "(") > substr_count($exp, ")")){
-    		return view('simplifier.error');
-    	}else{
-    		return view('simplifier.simplified', ['result' => $this->par($exp)]);
-        print($this->par($exp));
+        if(!$this->balanced($exp)){  
+            return view('simplifier.error');
+        }else{
+            return view('simplifier.simplified', ['result' => 'Sem erros']);
         }
-    }
-    public function par($exp){
-        $expPar = "";
-        $m = 0;
-        $n = 0;
-        for ($i=0; $i < strlen($exp); $i++) { 
-            if ($exp[$i] == "("){
-                $j[$m] = $i;
-                $m++;
-            }elseif ($exp[$i] == ")") {
-                $k[$n] = $i;
-                $n++;
-            }
-        }
-        $posA = $m-1;
-        $a = $j[$posA];
-        $b = $k[0];
-        for ($a; $a < $b-1; $a++) {
-            $expPar[$a] = $exp[$a+1];
-            
-        }
-        $expPar = $this->simplify($expPar);
-        // if($expPar == $exp){
-        //     $pos--;
-        // }
-        return $expPar;
+        
     }
     public function simplify($expPar){
         return $expPar;
     }
+
 }
